1f5612e4cdb32d40ebe4a181c2f9b0955546781b
ThereWolf <bdunham484@gmail.com>
2023-03-16 22:33:56 -0500

    add react/initial files

---
+++ script.js
@@ -0,0 +1,16 @@
+// select the div element with 'app' id
+const app = document.getElementById('app');
+
+//create a new h1 element
+const header = document.createElement('h1');
+
+//create a new text od for the H1 element
+const headerContent = document.createTextNode(
+    'Develop. Preview. Ship. 🚀',
+);
+
+//append the tedt to the h1 element
+header.appendChild(headerContent);
+
+//place the h1 element inside the div
+app.appendChild(header);
